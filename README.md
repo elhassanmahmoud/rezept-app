3db6af1c358dc7e4129aa5d60e1f2e144c7868a2
elhassanmahmoud <mahmoudlhassan63@gmail.com>
2025-04-14 13:13:27 +0200

    readme etc. hinzugefügt

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rezept-app
+# Rezept-App 🍝  **Angewandte Programmierung** im **SoSe 2025** an der HTW Berlin
+Die Rezept-App ist eine Anwendung zur Verwaltung und Anzeige von Rezepten.
+Ziel des Projekts ist es, eine Webanwendung zu entwickeln, mit der Nutzer Rezepte erstellen, bearbeiten, anzeigen und organisieren können.
+
+
--- src/main/java/elhassanmahmoud/rezeptapp/Rezept.java
+++ src/main/java/elhassanmahmoud/rezeptapp/Rezept.java
@@ -1,21 +1,23 @@
 package elhassanmahmoud.rezeptapp;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rezept {
 
-
+    @JsonProperty
     private Long id;
 
+    @JsonProperty
     private String titel;
+
+    @JsonProperty
     private String beschreibung;
-
-    public Rezept() {}
-
-    public Rezept(Long id, String titel, String beschreibung) {
-        this.id = id;
-        this.titel = titel;
-        this.beschreibung = beschreibung;
-    }
-
-    // Getter & Setter
 }
